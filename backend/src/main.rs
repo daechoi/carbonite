450bbdb40ac83d4717c30c4a1c3f86774a75fbe3
Dae Choi <daechoi@outlook.com>
2024-07-03 16:16:45 -0400

    adds request_id for correlation

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,10 +2,17 @@ use std::net::TcpListener;
 
 use carbonite::configuration::Settings;
 use carbonite::startup::run;
+use env_logger::Env;
 use sqlx::PgPool;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // `init` does call `set_logger`, so this is all we need to do.
+    // we are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable is not set.
+
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let settings = Settings::from_file("config/config.yaml").expect("failed to read configuration");
     let conn_pool = PgPool::connect(&settings.database.connection_string())
         .await
--- backend/src/routes/subscribe.rs
+++ backend/src/routes/subscribe.rs
@@ -12,6 +12,15 @@ struct FormData {
 
 #[post("subscriptions")]
 pub async fn subscribe(form: web::Form<FormData>, conn_pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+
+    log::info!(
+        "request_id: {} saving '{}' '{}' as new user",
+        request_id,
+        form.email,
+        form.name
+    );
+
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -25,9 +34,17 @@ pub async fn subscribe(form: web::Form<FormData>, conn_pool: web::Data<PgPool>) 
     .execute(conn_pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!(
+                "Subscribed {} with email {}: request_id: {}",
+                form.name,
+                form.email,
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            eprintln!("Failed to execute query: {:?}", e);
+            log::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
