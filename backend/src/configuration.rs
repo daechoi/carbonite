61050153b7696f34682a98154a62abfdacc8a341
Dae Choi <daechoi@outlook.com>
2024-07-02 23:31:38 -0400

    adds application configs

--- backend/src/configuration.rs
+++ backend/src/configuration.rs
@@ -1,2 +1,28 @@
-#[derive(serde::Deserialize)]
-pub struct Settings {}
+use config::{Config, ConfigError, File};
+use serde::Deserialize;
+
+/// Struct to hold the configuration settings
+#[derive(Deserialize)]
+pub struct Settings {
+    pub application_port: u16,
+    pub database: DatabaseSettings,
+}
+
+#[derive(Deserialize)]
+pub struct DatabaseSettings {
+    pub port: u16,
+    pub host: String,
+    pub database_user: String,
+    pub database_password: String,
+    pub database_name: String,
+}
+
+impl Settings {
+    pub fn from_file(file_name: &str) -> Result<Self, ConfigError> {
+        let config = Config::builder()
+            .add_source(File::with_name(file_name))
+            .build()?;
+
+        config.try_deserialize()
+    }
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,12 @@
 use std::net::TcpListener;
 
-use zero2prod::startup::run;
+use carbonite::configuration::Settings;
+use carbonite::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind to 8080");
+    let settings = Settings::from_file("config/config.yaml").expect("failed to read configuration");
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", settings.application_port))
+        .expect("Failed to bind to 8080");
     run(listener)?.await
 }
