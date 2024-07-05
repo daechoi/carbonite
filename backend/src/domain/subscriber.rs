e8949956496e868a37cb6f73f5b830b0aca5e0e8
Dae Choi <daechoi@outlook.com>
2024-07-05 11:07:26 -0400

    adds faker to generate random emails

--- backend/src/domain/subscriber.rs
+++ backend/src/domain/subscriber.rs
@@ -77,8 +77,13 @@ mod tests {
 
 #[cfg(test)]
 mod test {
+
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
+    use rand::{rngs::StdRng, SeedableRng};
+
     use super::SubscriberEmail;
-    use claim::assert_err;
+    use claim::{assert_err, assert_ok};
 
     #[test]
     fn empty_email_is_rejected() {
@@ -90,5 +95,28 @@ mod test {
     fn email_without_at_symbol_is_rejected() {
         let email = "ursula.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn valid_email_is_accepted() {
+        let email = SafeEmail().fake();
+        assert_ok!(SubscriberEmail::parse(email));
+    }
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+
+            let email = SafeEmail().fake_with_rng(&mut rng);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_accepted(email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(email.0.clone()).is_ok()
     }
 }
